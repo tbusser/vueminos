a4a1adee8f5e31d44fb9d85a7bd0789f8a617ab0
Thijs Busser <thijs.busser@iodigital.com>
2025-11-18 21:25:19 +0100

    Fix possible undefined as watch target
    There was a linter issue for the watch target as it could be undefined
    which is not allowed. By wrapping it in a method the error is fixed.

--- src/composables/useColorScheme.ts
+++ src/composables/useColorScheme.ts
@@ -1,10 +1,10 @@
-import { watch, type MaybeRefOrGetter } from 'vue';
+import { toValue, watch, type MaybeRefOrGetter } from 'vue';
 import type { ColorScheme } from '@/stores/settings';
 
 /* ========================================================================== */
 
 export function useColorScheme(scheme: MaybeRefOrGetter<ColorScheme | undefined>) {
-	watch(scheme, selectedScheme => {
+	watch(() => toValue(scheme), selectedScheme => {
 		document.documentElement.classList.toggle('dark-theme', selectedScheme === 'dark');
 		document.documentElement.classList.toggle('light-theme', selectedScheme === 'light');
 	}, {
