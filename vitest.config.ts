6e3b3f0c783dfdbf6c51d47b15321ca906d2ce40
Thijs Busser <thijs.busser@iodigital.com>
2026-04-27 10:38:41 +0200

    Fix vitest config
    The config was outdated since the NPM package was updated to the
    latest version.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,24 @@
 import { fileURLToPath } from 'node:url';
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import { mergeConfig } from 'vite';
+import { defineConfig, configDefaults } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
+
 import viteConfig from './vite.config';
 
 /* ========================================================================== */
 
-export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		test: {
-			browser: {
-				enabled: true,
-				instances: [
-					{ browser: 'chromium' }
-				],
-				provider: 'playwright'
-			},
-			environment: 'jsdom',
-			exclude: [...configDefaults.exclude, 'e2e/**'],
-			root: fileURLToPath(new URL('./', import.meta.url))
-		}
-	})
-);
+const vitestConfig = {
+	test: {
+		browser: {
+			enabled: true,
+			instances: [
+				{ browser: 'chromium' }
+			],
+			provider: playwright()
+		},
+		exclude: [...configDefaults.exclude, 'e2e/**'],
+		root: fileURLToPath(new URL('./', import.meta.url))
+	}
+};
+
+export default defineConfig(mergeConfig(viteConfig, vitestConfig));
