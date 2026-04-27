a11a3e4064f95b42e982a542814a289d639e32b6
Thijs Busser <thijs.busser@iodigital.com>
2026-04-27 10:20:33 +0200

    Update TurnInput creation in TurnScreen
    - The bonus shapes are taken into consideration
    - Fixed a type warning by explicitly splitting up the skipped and
      played turn inputs
    - Safeguarded that isTriple can't be set to true for non initial turns
      or for values where the checkbox is disabled

--- src/screens/TurnScreen.vue
+++ src/screens/TurnScreen.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 
 import BaseScreen from '@/components/BaseScreen.vue';
 import NumberDisplay from '@/components/NumberDisplay.vue';
@@ -13,11 +13,15 @@ import { useRules } from '@/composables/useRules';
 
 /* ========================================================================== */
 
+type BonusShape = 'bridge' | 'double' | 'hexagon';
+
+/* ========================================================================== */
+
 const emit = defineEmits<{
 	(event: 'turn-played', turn: ScoredTurnInput): void;
 }>();
 
-withDefaults(defineProps<{
+const props = withDefaults(defineProps<{
 	isInitialTurn?: boolean;
 	subtitle: string;
 	title: string;
@@ -30,6 +34,7 @@ withDefaults(defineProps<{
 const { calculateStartingStoneBonus, calculateTurnScore } = useRules();
 
 const isTripleStone = ref<boolean>(false);
+const selectedBonusShape = ref<BonusShape | undefined>(undefined);
 const tilesDrawn = ref<number>(0);
 const tileValue = ref<number | undefined>(undefined);
 
@@ -43,6 +48,16 @@ const isTripleStoneEnabled = computed<boolean>(() => {
 
 /* -------------------------------------------------------------------------- */
 
+watchEffect(() => {
+	if (isTripleStoneEnabled.value) return;
+
+	// When the played value cannot be a triple, the isTripleStone flag should
+	// be reset to false.
+	isTripleStone.value = false;
+});
+
+/* -------------------------------------------------------------------------- */
+
 /**
  * Creates a ScoredTurnInput object based on what the user has selected in the
  * turn screen.
@@ -50,15 +65,21 @@ const isTripleStoneEnabled = computed<boolean>(() => {
  * @returns A ScoredTurnInput object containing the turn input and the score.
  */
 function createScoredTurnInput(): ScoredTurnInput {
-	const turnInput: TurnInput = {
-		bonusBridge: false,
-		bonusDouble: false,
-		bonusHexagon: false,
-		tilesDrawn: tilesDrawn.value,
-		tilesPlayed: tileValue.value === undefined ? 0 : 1,
-		tileValue: tileValue.value,
-		triple: isTripleStoneEnabled.value && isTripleStone.value
-	};
+	const turnInput: TurnInput = tileValue.value === undefined
+		? {
+			tilesDrawn: tilesDrawn.value,
+			tilesPlayed: 0,
+			tileValue: undefined
+		}
+		: {
+			bonusBridge: selectedBonusShape.value === 'bridge',
+			bonusDouble: selectedBonusShape.value === 'double',
+			bonusHexagon: selectedBonusShape.value === 'hexagon',
+			tilesDrawn: tilesDrawn.value,
+			tilesPlayed: 1,
+			tileValue: tileValue.value,
+			triple: props.isInitialTurn && isTripleStone.value
+		};
 
 	return {
 		...turnInput,
@@ -67,6 +88,10 @@ function createScoredTurnInput(): ScoredTurnInput {
 }
 
 /* -------------------------------------------------------------------------- */
+
+function onToggleBonusScoring(value: string | string[] | undefined) {
+	selectedBonusShape.value = value as BonusShape;
+}
 
 function onNavigateForward() {
 	emit('turn-played', createScoredTurnInput());
@@ -106,16 +131,17 @@ function onToggleIsTripleStone(value: boolean) {
 
 				<ToggleButtonGroup
 					orientation="horizontal"
+					@update:model-value="onToggleBonusScoring"
 				>
-					<ShapeToggleButton id="bridge">
+					<ShapeToggleButton :id="'bridge' satisfies BonusShape">
 						<img src="@/assets/images/bridge.png" alt="Bridge" />
 						bridge
 					</ShapeToggleButton>
-					<ShapeToggleButton id="double">
+					<ShapeToggleButton :id="'double' satisfies BonusShape">
 						<img src="@/assets/images/double-sided.png" alt="Double" />
 						double
 					</ShapeToggleButton>
-					<ShapeToggleButton id="hexagon">
+					<ShapeToggleButton :id="'hexagon' satisfies BonusShape">
 						<img src="@/assets/images/hexagon.png" alt="Hexagon" />
 						hexagon
 					</ShapeToggleButton>
