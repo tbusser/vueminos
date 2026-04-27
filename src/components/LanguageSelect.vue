b80775fa3c7a0f6739cd3c20ba0aba3d9e6f75db
Thijs Busser <thijs.busser@iodigital.com>
2026-04-27 10:48:09 +0200

    Add id property to SelectOption
    The id is need so it can be used as the key in the v-for.

--- src/components/LanguageSelect.vue
+++ src/components/LanguageSelect.vue
@@ -8,16 +8,19 @@ const model = defineModel();
 
 const options = computed<SelectOption[]>(() => [
 	{
+		id: 'auto',
 		label: 'Auto',
 		selected: model.value === undefined,
 		value: undefined
 	},
 	{
+		id: 'en',
 		label: 'English',
 		selected: model.value === 'en',
 		value: 'en'
 	},
 	{
+		id: 'nl',
 		label: 'Dutch',
 		selected: model.value === 'nl',
 		value: 'nl'
