96dd8ffa2515e13b7167575dc7080060246a90ab
Thijs Busser <tbusser@gmail.com>
2026-05-05 23:04:19 +0200

    Add score aggregation to useGameScores
    Aggregating the score is a task better suited for useGameScores than
    it was for the rounds store.

--- src/composables/useGameScores.ts
+++ src/composables/useGameScores.ts
@@ -12,23 +12,57 @@ export function useGameScores() {
 	const roundsStore = useRoundsStore();
 	const playersStore = usePlayersStore();
 
-	/* ---------------------------------------------------------------------- */
-
 	const { pointsLimit } = storeToRefs(gameStore);
-	const { playerScores } = storeToRefs(roundsStore);
+	const { currentRound, completedRounds } = storeToRefs(roundsStore);
+
+	/* -- Score Aggregation ------------------------------------------------- */
+
+	const currentRoundScore = computed<ScorePerPlayer>(() => {
+		if (currentRound.value === undefined) return {};
+
+		return currentRound.value.playerStats.reduce((acc, playerStat) => {
+			acc[playerStat.id] = playerStat.score;
+			return acc;
+		}, {} as ScorePerPlayer);
+	});
+
+	const finishedRoundsScore = computed<ScorePerPlayer>(() => {
+		return completedRounds.value.reduce((acc, round) => {
+			(Object.keys(round.scores) as Id[]).forEach((playerId) => {
+				acc[playerId] = (acc[playerId] ?? 0) + round.scores[playerId];
+			});
+
+			return acc;
+		}, {} as ScorePerPlayer);
+	});
+
+	const totalScore = computed<ScorePerPlayer>(() => {
+		const keys = new Set<Id>([
+			...Object.keys(currentRoundScore.value) as Id[],
+			...Object.keys(finishedRoundsScore.value) as Id[]
+		]);
+
+		const result: ScorePerPlayer = {};
+		keys.forEach((key) => {
+			result[key] =
+				(currentRoundScore.value[key] ?? 0) + (finishedRoundsScore.value[key] ?? 0);
+		});
+
+		return result;
+	});
+
+	/* -- Game State -------------------------------------------------------- */
 
 	const hasReachedPointsLimit = computed<boolean>(() => {
-		const scores = Object.values(playerScores.value);
+		const scores = Object.values(totalScore.value);
 
 		return Math.max(...scores) >= pointsLimit.value;
 	});
 
-	/* ---------------------------------------------------------------------- */
-
 	const winningPlayerId = computed<Id | undefined>(() => {
 		if (!hasReachedPointsLimit.value) return undefined;
 
-		const [winner, ...players] = Object.entries(playerScores.value) as [Id, number][];
+		const [winner, ...players] = Object.entries(totalScore.value) as [Id, number][];
 
 		return players.reduce((winner, player) =>
 			player[1] > winner[1] ? player : winner
@@ -45,17 +79,19 @@ export function useGameScores() {
 
 	return {
 		/**
-		 * Indicates whether the points limit has been reached.
+		 * Indicates whether the points limit for the game has been reached.
 		 */
 		hasReachedPointsLimit,
 
 		/**
 		 * The points scored, per player, over the completed and current rounds.
 		 */
-		totalScore: playerScores,
+		totalScore,
 
 		/**
-		 * The player who has won the game.
+		 * The player who has won the game. Is undefined as long as the points
+		 * limit has not been reached or the winner is not found in the
+		 * players store.
 		 */
 		winner
 	};
---
+++ src/types/ScorePerPlayer.ts
@@ -0,0 +1,18 @@
+export {};
+
+/* ========================================================================== */
+
+declare global {
+	/**
+	 * Represents the score of each player. The keys are player IDs, and the
+	 * values are the number of points.
+	 *
+	 * @example {
+	 *     '123e4567-e89b-12d3-a456-426614174000': 4,
+	 *     ...
+	 * }
+	 */
+	type ScorePerPlayer = {
+		[id: Id]: number;
+	};
+}
