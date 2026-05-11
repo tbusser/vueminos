ea737c7b0dfa2fe940da460f4c73abec14bd67b5
Thijs Busser <tbusser@gmail.com>
2026-05-11 15:51:52 +0200

    refactor(screens): replace unnecessary v-html with text interpolation

--- src/screens/GameLimitScreen.vue
+++ src/screens/GameLimitScreen.vue
@@ -48,7 +48,7 @@ function onNavigateForward() {
 <template>
 	<AppScreen :title="$t('gameLimit.title')">
 		<MessageBox>
-			<div v-html="$t('gameLimit.description')" />
+			{{ $t('gameLimit.description') }}
 		</MessageBox>
 
 		<NumberDisplay :value="limit" />
