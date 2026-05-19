1a6dcd806e01a0de6ce5ddf89c2379ac65bd2e37
Thijs Busser <tbusser@gmail.com>
2026-05-19 22:52:59 +0200

    Add component to navigate through the history
    Though small the explicit name makes it easy to deduce what it does
    when encountered somewhere in a template.

---
+++ src/components/HistoryNavigation.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+	'go-back': [];
+	'go-forward': [];
+}>();
+
+defineProps<{
+	canGoBack: boolean;
+	canGoForward: boolean;
+}>();
+
+/* ========================================================================== */
+
+function onGoBack(): void {
+	emit('go-back');
+}
+
+function onGoForward(): void {
+	emit('go-forward');
+}
+</script>
+
+<template>
+	<div class="history-navigation">
+		<button
+			:disabled="!canGoBack"
+			type="button"
+			@click="onGoBack"
+		>
+			◁
+		</button>
+		<button
+			:disabled="!canGoForward"
+			type="button"
+			@click="onGoForward"
+		>
+			▷
+		</button>
+	</div>
+</template>
