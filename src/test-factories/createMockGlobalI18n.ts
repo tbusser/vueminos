1e19d3fd25dcef7071fca04213142dc4fb3ebc39
Thijs Busser <tbusser@gmail.com>
2026-05-22 00:57:44 +0200

    Remove old test factories

--- src/test-factories/createMockGlobalI18n.ts
+++
@@ -1,9 +0,0 @@
-import { vi } from 'vitest';
-
-/* ========================================================================== */
-
-export function createMockGlobalI18n(): void {
-	vi.mock('@/i18n', () => ({
-		useGlobalI18n: () => ({ t: (key: string) => key })
-	}));
-}
