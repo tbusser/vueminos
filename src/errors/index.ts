8555244fa3be6629b4556fcda3b3b66da8e78ad6
Thijs Busser <tbusser@gmail.com>
2026-06-05 09:36:09 +0200

    Remove error from index file

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,5 +1,4 @@
 export { CurrentRoundAlreadyExistsError } from './CurrentRoundAlreadyExists';
 export { NoCurrentRoundExistsError } from './NoCurrentRoundExists';
 export { PlayerIdNotFoundError } from './PlayerIdNotFound';
-export { RoundIdNotFoundError } from './RoundIdNotFound';
 export { TurnIdNotFoundError } from './TurnIdNotFound';
